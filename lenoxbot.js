fe8bafabda0d2b509e1f315a51f718045d149006
Taeyn <57112875+TaeynDevloper@users.noreply.github.com>
2019-12-04 13:28:37 +0100

    Update lenoxbot.js

--- lenoxbot.js
+++ lenoxbot.js
@@ -12,7 +12,7 @@ const LenoxBotSettingsProvider = require('./utils/SettingsProvider');
 require('moment-duration-format');
 
 // const shardId = process.env.SHARD_COUNT;
-const token = process.env.CLIENT_TOKEN;
+client.login(process.env.TOKEN);
 
 // settings.json checks
 if (!settings.owners.length) {
